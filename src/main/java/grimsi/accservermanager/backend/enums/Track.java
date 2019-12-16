421efef56621f4147eda3e9d14f66e4fc0cd89ec
Rainbowsixteen <emil.wikhede@gmail.com>
2019-12-16 19:36:06 +0100

    Adding all tracks as of ACC v1.1.2.

--- src/main/java/grimsi/accservermanager/backend/enums/Track.java
+++ src/main/java/grimsi/accservermanager/backend/enums/Track.java
@@ -2,13 +2,25 @@ package grimsi.accservermanager.backend.enums;
 
 public enum Track {
     barcelona,
+    barcelona_2019,
     brands_hatch,
+    brands_hatch_2019,
     hungaroring,
+    hungaroring_2019,
     misano,
+    misano_2019,
     monza,
+    monza_2019,
     nurburgring,
+    nurburgring_2019,
     paul_ricard,
+    paul_ricard_2019,
     silverstone,
+    silverstone_2019,
     spa,
-    zolder
+    spa_2019,
+    zandvoort,
+    zandvoort_2019,
+    zolder,
+    zolder_2019
 }

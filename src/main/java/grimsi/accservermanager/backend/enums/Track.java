85ce722ba98f25831082769eda1e794440d75515
Rainbowsixteen <emil.wikhede@gmail.com>
2019-12-27 16:36:00 +0100

    Added all tracks as of ACC v1.2.2 and also changed name on dependency group that caused a warning.

--- src/main/java/grimsi/accservermanager/backend/enums/Track.java
+++ src/main/java/grimsi/accservermanager/backend/enums/Track.java
@@ -1,10 +1,26 @@
 package grimsi.accservermanager.backend.enums;
 
 public enum Track {
-    nurburgring,
-    misano,
-    paul_ricard,
+    barcelona,
+    barcelona_2019,
+    brands_hatch,
+    brands_hatch_2019,
     hungaroring,
+    hungaroring_2019,
+    misano,
+    misano_2019,
+    monza,
+    monza_2019,
+    nurburgring,
+    nurburgring_2019,
+    paul_ricard,
+    paul_ricard_2019,
+    silverstone,
+    silverstone_2019,
+    spa,
+    spa_2019,
+    zandvoort,
+    zandvoort_2019,
     zolder,
-    monza
+    zolder_2019
 }
